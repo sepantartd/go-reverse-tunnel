0099e48ebd9972f3295ae91bd54b5a43b4b336a6
Sepanta <spnta40@gmail.com>
2026-09-17 23:17:27 +0330

    Refactor config tests to use config package

--- pkg/config/config_test.go
+++ pkg/config/config_test.go
@@ -1,53 +1,55 @@
-package config
+package config_test
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/sepantartd/go-reverse-tunnel/pkg/config"
+)
 
 func TestServerConfig_Validate(t *testing.T) {
 	tests := []struct {
 		name    string
-		cfg     ServerConfig
+		cfg     config.ServerConfig
 		wantErr bool
 	}{
 		{
 			name: "Valid Server Config",
-			cfg: ServerConfig{
-				ControlAddr: "0.0.0.0:8080",
-				Token:       "secret",
-				Clients: []ClientMapping{
-					{ClientID: "client1", Ports: []int{8081, 8082}},
-					{ClientID: "client2", Ports: []int{9090}},
+			cfg: config.ServerConfig{
+				ControlAddr:           "127.0.0.1:8080",
+				Token:                 "secret",
+				InsecureAllowPlaintext: true,
+				Clients: []config.ClientMapping{
+					{ClientID: "client1", Ports: []int{9001, 9002}},
 				},
 			},
 			wantErr: false,
 		},
 		{
-			name: "Invalid Control Address",
-			cfg: ServerConfig{
-				ControlAddr: "invalid-address",
+			name: "Missing TLS And Insecure Flag",
+			cfg: config.ServerConfig{
+				ControlAddr: "127.0.0.1:8080",
 				Token:       "secret",
+			},
+			wantErr: true,
+		},
+		{
+			name: "Invalid Control Address",
+			cfg: config.ServerConfig{
+				ControlAddr:           "invalid-addr",
+				Token:                 "secret",
+				InsecureAllowPlaintext: true,
 			},
 			wantErr: true,
 		},
 		{
 			name: "Port Collision Between Clients",
-			cfg: ServerConfig{
-				ControlAddr: ":8080",
-				Token:       "secret",
-				Clients: []ClientMapping{
-					{ClientID: "client1", Ports: []int{8081}},
-					{ClientID: "client2", Ports: []int{8081}},
-				},
-			},
-			wantErr: true,
-		},
-		{
-			name: "Duplicate ClientID",
-			cfg: ServerConfig{
-				ControlAddr: ":8080",
-				Token:       "secret",
-				Clients: []ClientMapping{
-					{ClientID: "client1", Ports: []int{8081}},
-					{ClientID: "client1", Ports: []int{8082}},
+			cfg: config.ServerConfig{
+				ControlAddr:           "127.0.0.1:8080",
+				Token:                 "secret",
+				InsecureAllowPlaintext: true,
+				Clients: []config.ClientMapping{
+					{ClientID: "client1", Ports: []int{8000}},
+					{ClientID: "client2", Ports: []int{8000}},
 				},
 			},
 			wantErr: true,
@@ -58,7 +60,7 @@ func TestServerConfig_Validate(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			err := tt.cfg.Validate()
 			if (err != nil) != tt.wantErr {
-				t.Errorf("Validate() error = %v, wantErr %v", err, tt.wantErr)
+				t.Errorf("ServerConfig.Validate() error = %v, wantErr %v", err, tt.wantErr)
 			}
 		})
 	}
@@ -67,35 +69,27 @@ func TestServerConfig_Validate(t *testing.T) {
 func TestClientConfig_Validate(t *testing.T) {
 	tests := []struct {
 		name    string
-		cfg     ClientConfig
+		cfg     config.ClientConfig
 		wantErr bool
 	}{
 		{
 			name: "Valid Client Config",
-			cfg: ClientConfig{
-				ServerAddr: "127.0.0.1:8080",
-				ClientID:   "client1",
-				Token:      "secret",
+			cfg: config.ClientConfig{
+				ServerAddr:            "127.0.0.1:8080",
+				LocalAddr:             "127.0.0.1:3000",
+				ClientID:              "client1",
+				Token:                 "secret",
+				InsecureAllowPlaintext: true,
 			},
 			wantErr: false,
 		},
 		{
-			name: "Invalid Server Address",
-			cfg: ClientConfig{
-				ServerAddr: "127.0.0.1",
-				ClientID:   "client1",
-				Token:      "secret",
-			},
-			wantErr: true,
-		},
-		{
-			name: "TLS Missing KeyFile",
-			cfg: ClientConfig{
+			name: "Missing TLS and Plaintext Flag",
+			cfg: config.ClientConfig{
 				ServerAddr: "127.0.0.1:8080",
+				LocalAddr:  "127.0.0.1:3000",
 				ClientID:   "client1",
 				Token:      "secret",
-				EnableTLS:  true,
-				CertFile:   "cert.pem",
 			},
 			wantErr: true,
 		},
@@ -105,7 +99,7 @@ func TestClientConfig_Validate(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			err := tt.cfg.Validate()
 			if (err != nil) != tt.wantErr {
-				t.Errorf("Validate() error = %v, wantErr %v", err, tt.wantErr)
+				t.Errorf("ClientConfig.Validate() error = %v, wantErr %v", err, tt.wantErr)
 			}
 		})
 	}
