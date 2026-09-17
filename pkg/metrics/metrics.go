66b09a072b5f61dc2de4f9ac3375ae01f0c67073
Sepanta <spnta40@gmail.com>
2026-09-18 00:51:46 +0330

    Update metrics.go

--- pkg/metrics/metrics.go
+++ pkg/metrics/metrics.go
@@ -2,20 +2,23 @@ package metrics
 
 import (
 	"net/http"
+	"sync"
 
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 var (
+	once sync.Once
+
 	ActiveClients = prometheus.NewGauge(prometheus.GaugeOpts{
 		Name: "tunnel_active_clients",
-		Help: "Current number of active connected clients",
+		Help: "Number of active connected clients",
 	})
 
 	ActiveStreams = prometheus.NewGauge(prometheus.GaugeOpts{
 		Name: "tunnel_active_streams",
-		Help: "Current number of active multiplexed streams",
+		Help: "Number of active multiplexed streams",
 	})
 
 	BytesTransferred = prometheus.NewCounterVec(prometheus.CounterOpts{
@@ -27,13 +30,21 @@ var (
 		Name: "tunnel_auth_failures_total",
 		Help: "Total number of failed authentication attempts",
 	})
+
+	RateLimitTriggers = prometheus.NewCounter(prometheus.CounterOpts{
+		Name: "tunnel_rate_limit_triggers_total",
+		Help: "Total number of connections dropped due to rate limiting",
+	})
 )
 
 func Register() {
-	prometheus.MustRegister(ActiveClients)
-	prometheus.MustRegister(ActiveStreams)
-	prometheus.MustRegister(BytesTransferred)
-	prometheus.MustRegister(AuthFailures)
+	once.Do(func() {
+		prometheus.MustRegister(ActiveClients)
+		prometheus.MustRegister(ActiveStreams)
+		prometheus.MustRegister(BytesTransferred)
+		prometheus.MustRegister(AuthFailures)
+		prometheus.MustRegister(RateLimitTriggers)
+	})
 }
 
 func Handler() http.Handler {
